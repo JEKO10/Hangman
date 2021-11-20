8c92c3c40edbe1cdbc60274295a350319ec2d34e
Aleksa Bubanja <aleksa3b@gmail.com>
2021-11-20 21:03:36 +0100

    showing words if they are correct

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const wordContainer = document.querySelector(".wordContainer");
 const buttonContainer = document.querySelector(".buttonContainer");
-const answer = "jeko";
+const answer = "banana";
+const wordState = [];
 
 const showWordLength = () => {
   for (let i = 0; i < answer.length; i++) {
     const button = document.createElement("button");
+    // button.setAttribute("class", "hiddenColor");
     button.textContent = "_";
-    wordContainer.appendChild(button);
+    wordContainer.append(button);
+    wordState.push("_");
   }
 };
 
@@ -16,9 +19,30 @@ const createKeyboard = () => {
 
   for (let i = start.charCodeAt(); i <= end.charCodeAt(); i++) {
     const alphabet = document.createElement("button");
+    // alphabet.setAttribute("class", "letterColor");
     alphabet.textContent = String.fromCharCode(i);
+    alphabet.addEventListener("click", guessLetter);
+    alphabet.addEventListener("click", () => {
+      alphabet.style.pointerEvents = "none";
+    });
     buttonContainer.append(alphabet);
   }
+};
+
+const guessLetter = (e) => {
+  // e.target.setAttribute("class", "grayColor");
+  const wordButtons = document.querySelectorAll(".wordContainer button");
+  const currentGuess = e.target.textContent;
+
+  for (let i = 0; i < answer.length; i++) {
+    if (currentGuess === answer[i]) {
+      const corretElement = wordButtons[i];
+      corretElement.textContent = currentGuess;
+      wordState[i] = currentGuess;
+    }
+  }
+
+  console.log(wordState);
 };
 
 createKeyboard();
